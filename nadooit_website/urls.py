e326d626f31fefa270a420a6fb548838473d5d56
Christoph Backhaus <christoph.backhaus@nadooit.de>
2022-08-10 20:09:26 +0200

    Added pages
    Added Pages:
    
    Impressum, Datenschutz und Onlinebüro
    
    Navbar mit den jeweiligen optionen

--- nadooit_website/urls.py
+++ nadooit_website/urls.py
@@ -4,4 +4,6 @@ from . import views
 #This is where the urls are placed
 urlpatterns = [
                   path('', views.index, name='index'),
+                  path('impressum', views.impressum, name='impressum'),
+                  path('datenschutz', views.datenschutz, name='datenschutz'),
               ]
--- nadooit_website/views.py
+++ nadooit_website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+def impressum(request):
+    return render(request, 'impressum.html', {})
+def datenschutz(request):
+    return render(request, 'datenschutz.html', {})
