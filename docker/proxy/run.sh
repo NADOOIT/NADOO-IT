cca0026c43ca26d928b5829735acec483ea1342e
Christoph Backhaus <christoph.backhaus@nadooit.de>
2022-10-10 14:56:24 +0200

    fix run.sh

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "Checking for dhparams.pem"
 
-if [ ! -f "/vol/proxy/ssl-dhparams.pem"]; then
+if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
     echo "dhparams.pem does not exist, generating"
     openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
 fi
