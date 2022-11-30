0faf908392fae4d941e967f46580ced045f5af23
Christoph Backhaus <christoph.backhaus@nadooit.de>
2022-11-30 15:07:06 +0100

    preparing for testing
    Getting error because cockroachdb seems not correctly setup for giving a test db

---
+++ app/nadooit_os/test_services.py
@@ -0,0 +1,43 @@
+import pytest
+from nadooit_crm.models import Customer
+from nadooit_program.models import Program
+from nadooit_program_ownership_system.models import CustomerProgram
+from nadooit_auth.models import User
+
+from nadooit_os.services import (
+    check__user__is__customer_program_manager__for__customer_prgram,
+)
+
+# A pytest fixure that returns a user object
+@pytest.fixture
+def user():
+    return User.objects.create(
+        username="test",
+        display_name="test",
+    )
+
+
+# A pytest fixure that returns a customer program object
+@pytest.fixture
+def customer_program():
+    return CustomerProgram.objects.create(
+        name="test",
+        description="test",
+        customer=Customer.objects.create(
+            name="test",
+        ),
+        program=Program.objects.create(
+            name="test",
+            description="test",
+        ),
+    )
+
+
+@pytest.mark.django_db
+def test_check__user__is__customer_program_manager__for__customer_prgram(
+    user, customer_program
+):
+    # Arrange
+    # Act
+    # Assert
+    assert check__user__is__customer_program_manager__for__customer_prgram() == True
