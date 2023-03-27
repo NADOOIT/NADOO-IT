d91146672f8b58eedd931572c3e9c757c2cd1e95
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-03-27 19:13:34 +0200

    fix for CSRF_TRUSTED_ORIGINS missing

--- app/nadooit/settings.py
+++ app/nadooit/settings.py
@@ -48,6 +48,8 @@ DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
 # Example: DJANGO_ALLOWED_HOSTS= "localhost, nadooit.de,
 ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+
 # Application definition
 # This is the list of installed apps. If a new app is added, it must be added here.
 # The order of the apps is important. The apps are loaded in the order they are listed here.
