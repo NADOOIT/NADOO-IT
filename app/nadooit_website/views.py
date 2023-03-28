10de1bcbdc407ca8f67f1c4a07f9600da9080654
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-03-28 10:56:57 +0200

    Added new model that "tracks" visits to individual pages without any user data

--- app/nadooit_website/views.py
+++ app/nadooit_website/views.py
@@ -1,20 +1,56 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from nadooit_auth.models import User
+from nadooit_website.models import Visit
+
+
+def user_is_staf(user: User) -> bool:
+    return user.is_staff
 
 
 # Create your views here.
 def index(request):
+
+    # create a visit object for the index page
+    visit = Visit(site="Index")
+    # save the visit
+    visit.save()
+
     return render(request, "nadooit_website/index.html", {"page_title": "Home"})
 
 
 def impressum(request):
+
+    # create a visit object
+    visit = Visit(site="Impressum")
+    # save the visit
+    visit.save()
+
     return render(
         request, "nadooit_website/impressum.html", {"page_title": "Impressum"}
     )
 
 
 def datenschutz(request):
+
+    # create a visit object
+    visit = Visit(site="Datenschutz")
+    # save the visit
+    visit.save()
+
     return render(
         request,
         "nadooit_website/datenschutz.html",
         {"page_title": "Datenschutzerklärung"},
+    )
+
+
+@login_required(login_url="/auth/login-user")
+@user_passes_test(user_is_staf, login_url="/auth/login-user")
+def statistics(request):
+    return render(
+        request,
+        "nadooit_website/statistics.html",
+        {"page_title": "Statistiken", "visits": Visit.objects.all()},
     )
