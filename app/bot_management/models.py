6598094d151109c2cf3f0bf5cd99719c4f962a6f
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-06-09 19:06:36 +0200

    models for bot with choices
    I decided against adding encryption for the sensitiv information for new. This needs to be changed in the future. Currenlty did not find a activly maintaind project with encrytion.

--- app/bot_management/models.py
+++ app/bot_management/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from nadooit_crm.models import Customer
+
+# Options
+PLATFORM_CHOICES = [
+    ("telegram", "Telegram"),
+    ("facebook", "Facebook Messenger"),
+    ("whatsapp", "WhatsApp"),
+    # add more platforms as needed
+]
+
+
 # Create your models here.
+class Bot(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class BotPlatform(models.Model):
+    bot = models.ForeignKey(Bot, on_delete=models.CASCADE)
+    platform = models.CharField(max_length=50, choices=PLATFORM_CHOICES)
+    access_token = models.CharField(max_length=100)
+    api_key = models.CharField(max_length=100, unique=True)
+    secret_url = models.CharField(max_length=200, unique=True)
