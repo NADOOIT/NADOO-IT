19bad41bdafb6356b2e242b1051defb850fab8d8
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-07-12 11:37:18 +0200

    non functional, adding checking if user is allowed to use bot

--- app/bot_management/plattforms/telegram/bot.py
+++ app/bot_management/plattforms/telegram/bot.py
@@ -1,6 +1,10 @@
 from typing import Dict, Callable
 import os
 import importlib
+
+from django.shortcuts import get_object_or_404
+from nadooit_hr.models import *
+from nadooit_auth.models import *
 from bot_management.plattforms.telegram.exceptions import (
     InvalidMessageDataError,
     BotPlatformNotFoundError,
@@ -82,52 +86,90 @@ def process_message(message_id, token: str, bot_name):
 
     message = TelegramMessage.objects.get(message_id=message_id)
 
+    # With the provided token get the bot and then check if the sender of the message is an employee of the company the bot belongs to
+    # To do that the Bot has a field called customer
+    # Then there is a field called TelegramConnector that has two fields: auth_user and telegram_user using these we can check if the NADOOIT user is also an employee of the company
+    # The relation between the NADOOIT user and the company is stored in the EmployeeContract model. This model has a field for the customer and an employee that has this connection user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # With the provided token get the bot and then check if the sender of the message is an employee of the company the bot belongs to
+    BotPlatform_or_httpresponse = get_object_or_404(BotPlatform, access_token=token)
+
+    if BotPlatform_or_httpresponse is HttpResponse:
+        send_message(
+            chat_id=message.chat.id,
+            text=f"Invalid token",
+            token=token,
+        )
+
+    bot = BotPlatform_or_httpresponse.bot
+
+    customer = bot.customer
+
+    telegram_connector = get_object_or_404(
+        TelegramConnector, telegram_user=message.from_user
+    )
+    auth_user = telegram_connector.auth_user
+
+    employee = get_object_or_404(Employee, user=auth_user)
+    employee_contract = get_object_or_404(
+        EmployeeContract, employee=employee, customer=customer, is_active=True
+    )
+
     # Check if message has text and if it's a command
-    if message.text is not None and message.text.startswith("/"):
-        # Split the command and the arguments
-        command, args = (
-            message.text.split(" ", 1) if " " in message.text else (message.text, "")
-        )
-
-        """ 
-        # If user is in a conversation
-        if message.conversation_state:
-            # If the command sent by user is same as the one in conversation
-            if message.conversation_state.command == command:
-                # Ask if user wants to continue conversation
-                # TODO: Implement the way to ask user and handle the response
-            else:
-                # If different command is sent, ask if user wants to pause the current conversation
-                # TODO: Implement the way to ask user and handle the response
-
-        paused_conversations = Message.objects.filter(
-            Q(conversation_state__status='paused') & Q(conversation_state__command=command)
-        )
-        # If there are paused conversations for this command
-        if paused_conversations:
-            # Ask user if they want to continue one of these conversations
-            # TODO: Implement the way to ask user and handle the response
-        """
-
-        # Check if command exists in the registry and run it
-        if command in command_registry:
-            command_func, _ = command_registry[
-                command
-            ]  # We don't need the description here, so we just ignore it
-            command_func(message, token, args)
-
-        # If command is not registered, you can send a default message or do nothing
-        else:
-            send_message(
-                chat_id=message.chat.id,
-                text=f"Unknown command: {command}",
-                token=token,
-            )
-
-    # If message is not a command, you can send a default message or do nothing
-    if message.text is not None and not message.text.startswith("/"):
+    if message.text is None or not message.text.startswith("/"):
+        # If message is not a command, you can send a default message or do nothing
         send_message(
             chat_id=message.chat.id,
             text=f"Unknown message: {message.text}",
             token=token,
         )
+    else:
+        # If the sender is an employee of the company, process the message using the command_registry
+        if employee_contract is not None and not HttpResponse:
+            # Split the command and the arguments
+            command, args = (
+                message.text.split(" ", 1)
+                if " " in message.text
+                else (message.text, "")
+            )
+
+            """ 
+            # If user is in a conversation
+            if message.conversation_state:
+                # If the command sent by user is same as the one in conversation
+                if message.conversation_state.command == command:
+                    # Ask if user wants to continue conversation
+                    # TODO: Implement the way to ask user and handle the response
+                else:
+                    # If different command is sent, ask if user wants to pause the current conversation
+                    # TODO: Implement the way to ask user and handle the response
+
+            paused_conversations = Message.objects.filter(
+                Q(conversation_state__status='paused') & Q(conversation_state__command=command)
+            )
+            # If there are paused conversations for this command
+            if paused_conversations:
+                # Ask user if they want to continue one of these conversations
+                # TODO: Implement the way to ask user and handle the response
+            """
+
+            # Check if command exists in the registry and run it
+            if command in command_registry:
+                command_func, _ = command_registry[
+                    command
+                ]  # We don't need the description here, so we just ignore it
+                command_func(message, token, args)
+
+            # If command is not registered, you can send a default message or do nothing
+            else:
+                send_message(
+                    chat_id=message.chat.id,
+                    text=f"Unknown command: {command}",
+                    token=token,
+                )
+
+        else:
+            send_message(
+                chat_id=message.chat.id,
+                text=f"You are not an employee of this company and therefore not allowed to use this bot.",
+                token=token,
+            )
