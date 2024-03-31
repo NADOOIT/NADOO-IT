7b2c273eb5f1d4cb746200408905c68393436695
Christoph Backhaus <106314951+NADOOITChristophBa@users.noreply.github.com>
2024-03-31 16:25:42 +0200

    Autoerstattung entfernt und steuerbare aktionen hinzgefügt

--- app/nadooit_api_executions_system/admin.py
+++ app/nadooit_api_executions_system/admin.py
@@ -6,8 +6,27 @@
 # License: TBD
 
 from django.contrib import admin
+from .models import CustomerProgramExecution
 
-from .models import *
+class CustomerProgramExecutionAdmin(admin.ModelAdmin):
+    actions = ['delete_with_refund', 'delete_without_refund']
 
-# Register your models here.
-admin.site.register(CustomerProgramExecution)
+    def delete_with_refund(self, request, queryset):
+        """
+        Löscht die ausgewählten CustomerProgramExecution-Objekte und erstattet die Zeit dem zugehörigen Zeitkonto.
+        """
+        for obj in queryset:
+            customer_program = obj.customer_program
+            customer_program.time_account.time_balance_in_seconds += obj.program_time_saved_in_seconds
+            customer_program.time_account.save()
+            obj.delete()
+    delete_with_refund.short_description = "Löschen mit Erstattung"
+
+    def delete_without_refund(self, request, queryset):
+        """
+        Löscht die ausgewählten CustomerProgramExecution-Objekte ohne Erstattung der Zeit.
+        """
+        queryset.delete()
+    delete_without_refund.short_description = "Löschen ohne Erstattung"
+
+admin.site.register(CustomerProgramExecution, CustomerProgramExecutionAdmin)
